e324895729127a1dd81e071053d20d8c2fe0c092
Janis Gailis <janis.gailis@uia.no>
2023-01-25 01:49:47 +0100

    funfacts return

--- funfacts/funfacts.go
+++ funfacts/funfacts.go
@@ -2,7 +2,7 @@ package funfucts
 
 /**
   Implementer funfacts-funksjon:
-    GetFunFacts(about string)
+    GetFunFacts(about string) []string
       hvor about kan ha en av tre testverdier, -
         sun, luna eller terra
 
