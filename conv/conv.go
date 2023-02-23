d41907283270f22aafe2a8820ff2c4168b669e48
Janis Gailis <janis.gailis@uia.no>
2023-02-23 23:34:56 +0100

    conv.go

--- conv/conv.go
+++ conv/conv.go
@@ -15,8 +15,19 @@ func FarhenheitToCelsius(value float64) float64 {
 	// Du skal ikke formattere float64 i denne funksjonen
 	// Gjør formattering i main.go med fmt.Printf eller
 	// lignende
-	return 56.67
+	/*
+	if value == 32 {
+		return 0
+	} else {
+		return 56.67
+	}
+	*/
+	return (value - 32.0)*(5.0/9.0)
+
 }
 
 // De andre konverteringsfunksjonene implementere her
 // ...
+func CelsiusToFahrenheit(value float64) float64 {
+  return value*(9.0/5.0) + 32
+}
